79c4e3ab47f9cb0700a4e80d7a9521bf50cf731f
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2024-05-02 21:07:22 -0400

    Add CAN communication and send periodic heartbeat message

--- common.h
+++ common.h
@@ -11,6 +11,10 @@ static inline void BLUE_LED_OFF(void) {
     LATA2 = 1;
 }
 
+static inline void BLUE_LED_TOGGLE(void) {
+    LATA2 ^= 1;
+}
+
 static inline void GREEN_LED_ON(void) {
     LATA3 = 0;
 }
@@ -19,12 +23,20 @@ static inline void GREEN_LED_OFF(void) {
     LATA3 = 1;
 }
 
+static inline void GREEN_LED_TOGGLE(void) {
+    LATA3 ^= 1;
+}
+
 static inline void RED_LED_ON(void) {
     LATA4 = 0;
 }
 
 static inline void RED_LED_OFF(void) {
     LATA4 = 1;
+}
+
+static inline void RED_LED_TOGGLE(void) {
+    LATA4 ^= 1;
 }
 
 #endif
--- main.c
+++ main.c
@@ -10,8 +10,30 @@ static inline void SET_ACCEL_I2C_ADDR(uint8_t addr) {
     LATC2 = (addr == 0x19); // SA0 pin of FXLS set LSB of 7-bit I2C Address
 }
 
+static void can_msg_handler(const can_msg_t *msg) {}
+
 int main(void) {
     SYSTEM_Initialize();
+
+    // Enable global interrupts
+    INTCON0bits.GIE = 1;
+
+    // Set up CAN TX
+    TRISC1 = 0;
+    RC1PPS = 0x33;
+
+    // Set up CAN RX
+    TRISC0 = 1;
+    ANSELC0 = 0;
+    CANRXPPS = 0b00010000;
+
+    // Set up CAN module
+    can_timing_t can_setup;
+    can_generate_timing_params(_XTAL_FREQ, &can_setup);
+    can_init(&can_setup, can_msg_handler);
+
+    // Set up timer 0 for millis
+    timer0_init();
 
     TRISA2 = 0; // Blue LED output enable
     TRISA3 = 0; // Green LED output enable
@@ -21,16 +43,36 @@ int main(void) {
     SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     i2c_init(0b000); // I2C at 100 kHz
 
+    uint32_t last_millis = 0;
+
     while (1) {
+        if ((millis() - last_millis) < 500) {
+            continue;
+        }
+        last_millis = millis();
+
         CLRWDT();
 
         uint8_t whoami = fxls_get_whoami();
         uint8_t prod_rev = fxls_get_prod_rev();
 
-        BLUE_LED_OFF();
-        for (unsigned int i = 0; i < 60000; i++) {}
+        BLUE_LED_TOGGLE();
 
-        BLUE_LED_ON();
-        for (unsigned int i = 0; i < 60000; i++) {}
+        can_msg_t msg;
+        build_board_stat_msg(millis(), E_NOMINAL, NULL, 0, &msg);
+        can_send(&msg);
+    }
+}
+
+static void __interrupt() interrupt_handler() {
+    if (PIR5) {
+        can_handle_interrupt();
+    }
+
+    // Timer0 has overflowed - update millis() function
+    // This happens approximately every 500us
+    if (PIE3bits.TMR0IE == 1 && PIR3bits.TMR0IF == 1) {
+        timer0_handle_interrupt();
+        PIR3bits.TMR0IF = 0;
     }
 }
