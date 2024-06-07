8f6393335e7a808242bd533f9f85653816380b21
Sreya Roy Chowdhury <s4roycho@uwaterloo.ca>
2024-06-07 13:25:47 -0400

    updated fxls_init

--- fxls8974cf.c
+++ fxls8974cf.c
@@ -4,18 +4,32 @@
 #include <stdio.h>
 
 void fxls_init(void) {
-    // Disable buffer mode
     i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
+
+    // Disable buffer mode
     uint8_t buf_config1 = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_BUF_CONFIG1);
     buf_config1 &= ~BUF_MODE_MASK;  // Clear BUF_MODE1 and BUF_MODE0 bits
-    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     i2c_write_reg8(FXLS_I2C_ADDR, FXLS_BUF_CONFIG1, buf_config1);
+    
+    // Enable I2C
+    uint8_t sens_config1 = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG1);
+    sens_config1 |= SENS_CONFIG1_MASK;
+    i2c_write_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG1, sens_config1);
+    
+    // Set ODR to 100Hz: set bits 7:4 to 0101
+    uint8_t sens_config3 = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG3);
+    sens_config3 &= 0x0F;
+    sens_config3 |= 0b01010000;
+    i2c_write_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG3, sens_config3);
+    
+    // Enable interrupts - 7th bit data ready interrupt
+    uint8_t int_en = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_INT_EN);
+    int_en |= 0b10000000;
+    i2c_write_reg8(FXLS_I2C_ADDR, FXLS_INT_EN, int_en);
 
     // Enable data ready interrupt - is this even needed?
-    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     uint8_t sens_config4 = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG4);
     sens_config4 |= DRDY_EN_MASK;  // Set DRDY_EN bit
-    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     i2c_write_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG4, sens_config4);
 }
 
--- fxls8974cf.h
+++ fxls8974cf.h
@@ -8,11 +8,20 @@
 #define INT_STATUS_REG 0x00
 #define SRC_DRDY_MASK 0x80  // 10000000 in binary, bit 7 mask
 
-#define FXLS_BUF_CONFIG1 0x26
-#define BUF_MODE_MASK 0x03
+#define FXLS_SENS_CONFIG1 0x15
+#define SENS_CONFIG1_MASK 0x01
+
+#define FXLS_SENS_CONFIG2 0x16
+
+#define FXLS_SENS_CONFIG3 0x17
+
+#define FXLS_INT_EN 0x20
 
 #define FXLS_SENS_CONFIG4 0x18
 #define DRDY_EN_MASK 0x01
+
+#define FXLS_BUF_CONFIG1 0x26
+#define BUF_MODE_MASK 0x03
 
 #define FXLS_REG_OUT_X_MSB 0x05
 #define FXLS_REG_OUT_X_LSB 0x04
