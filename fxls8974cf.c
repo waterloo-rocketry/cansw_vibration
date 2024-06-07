620d508c73e59b6fafa206c2f97f5a2a100def12
Sreya Roy Chowdhury <s4roycho@uwaterloo.ca>
2024-06-06 20:09:07 -0400

    newest changes

--- fxls8974cf.c
+++ fxls8974cf.c
@@ -5,13 +5,17 @@
 
 void fxls_init(void) {
     // Disable buffer mode
+    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     uint8_t buf_config1 = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_BUF_CONFIG1);
     buf_config1 &= ~BUF_MODE_MASK;  // Clear BUF_MODE1 and BUF_MODE0 bits
+    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     i2c_write_reg8(FXLS_I2C_ADDR, FXLS_BUF_CONFIG1, buf_config1);
 
     // Enable data ready interrupt - is this even needed?
+    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     uint8_t sens_config4 = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG4);
     sens_config4 |= DRDY_EN_MASK;  // Set DRDY_EN bit
+    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     i2c_write_reg8(FXLS_I2C_ADDR, FXLS_SENS_CONFIG4, sens_config4);
 }
 
@@ -24,17 +28,27 @@ uint8_t fxls_get_whoami(void) {
 }
 
 int data_ready() {
+    // INT_STATUS_REG = 0x00
+    i2c_init(0b000); // I2C at 100 kHz SET_ACCEL_I2C_ADDR(FXLS_I2C_ADDR);
     uint8_t status = i2c_read_reg8(FXLS_I2C_ADDR, INT_STATUS_REG);
     return (status & SRC_DRDY_MASK) ? 1 : 0;
 }
 
 void fxls_read_accel_data(int16_t *x, int16_t *y, int16_t *z) {
-    uint8_t data[6];
+    uint8_t x_lsb, x_msb, y_lsb, y_msb, z_lsb, z_msb;
     
-    // Read 6 bytes of accelerometer data starting from the X-axis MSB register
-    i2c_read_reg_multibyte(FXLS_I2C_ADDR, FXLS_REG_OUT_X_LSB, data, 6);
-    
-    *x = (int16_t)((data[1] << 8) | data[0]);
-    *y = (int16_t)((data[3] << 8) | data[2]);
-    *z = (int16_t)((data[5] << 8) | data[4]);
+    i2c_init(0b000); // Initialize I2C at 100 kHz
+
+    // Read each byte of accelerometer data individually
+    x_lsb = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_REG_OUT_X_LSB);
+    x_msb = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_REG_OUT_X_MSB);
+    y_lsb = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_REG_OUT_Y_LSB);
+    y_msb = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_REG_OUT_Y_MSB);
+    z_lsb = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_REG_OUT_Z_LSB);
+    z_msb = i2c_read_reg8(FXLS_I2C_ADDR, FXLS_REG_OUT_Z_MSB);
+
+    // Combine the LSB and MSB for each axis
+    *x = (int16_t)((x_msb << 8) | x_lsb);
+    *y = (int16_t)((y_msb << 8) | y_lsb);
+    *z = (int16_t)((z_msb << 8) | z_lsb);
 }
