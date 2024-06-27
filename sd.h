c662b4fea4aab6ad0653fade0370666284bb87bb
ishman1-admin <ishman03@gmail.com>
2024-06-27 18:49:55 -0400

    spi header fix

--- sd.h
+++ sd.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-void init_spi();
 uint8_t init_sd_card2();
 
 void sd_card_log_to_file(const char *buffer, uint16_t len);
--- spi.h
+++ spi.h
@@ -1,26 +1,22 @@
 #ifndef SPI_H
 #define	SPI_H
 
-
 #define SPI_POLL_TIMEOUT 2000
 #define SPI_MAX_TXWE_CNT 1000
 #define SPI_MAX_RXRE_CNT 1000
 
-
-
 #include <stdbool.h>
 #include <stdint.h>
 
-void spi_init ();
+void spi1_init ();
 
-void spi_send(uint8_t data);
+void spi1_send(uint8_t data);
 
-void spi_send_buffer(uint8_t *data, uint16_t data_len);
+void spi1_send_buffer(uint8_t *data, uint16_t data_len);
 
-void spi_read_buffer(uint8_t *data, uint16_t data_len);
+void spi1_read_buffer(uint8_t *data, uint16_t data_len);
 
-uint8_t spi2_read(void);
-
+uint8_t spi1_read(void);
 
 
 
